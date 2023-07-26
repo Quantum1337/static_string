83d8911eb00ed259f629c1466bc8456538d70ad9
Quantum1337 <mail@christian-boy.de>
2023-07-26 19:42:32 +0200

    Added licence information on every file.

--- src/Implementation/basic_string_Assert.hpp
+++ src/Implementation/basic_string_Assert.hpp
@@ -1,7 +1,7 @@
 /*
 MIT License
 
-Copyright (c) 2023 Quantum1337
+Copyright (c) 2023 Quantum1337 (Christian Knupfer)
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
